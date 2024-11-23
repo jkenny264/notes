8911433c82ed9e7bab05f0af9e5055584d6a610f
jkenny264 <johnkenny264@gmail.com>
2024-11-23 02:18:54 -0500

    Update lab12.sql

--- docs/lab12.sql
+++ docs/lab12.sql
@@ -16,15 +16,10 @@ FROM assignments
 WHERE due_date>CURRENT_DATE
 ORDER BY due_date ASC;
 
-
-
 -- 4
 SELECT status, COUNT(*)
 FROM assignments
 GROUP BY status;
-
-
-
 
 -- 5
 SELECT length(course_name) AS len, *
@@ -35,15 +30,12 @@ ORDER BY len DESC;
 SELECT upper(course_name) AS "COURSE NAMES"
 FROM courses;
 
-
 -- 7
 SELECT title, due_date
 FROM assignments
 WHERE due_date LIKE '%-09-%';
 
-
 -- 8
-
 SELECT *
 FROM assignments
 WHERE due_date IS NULL;
