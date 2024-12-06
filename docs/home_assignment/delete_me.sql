7b8f30827f59b22e787c1716ea96866acf232a2c
jkenny264 <johnkenny264@gmail.com>
2024-12-06 18:52:19 -0500

    Update delete_me.sql

--- docs/home_assignment/delete_me.sql
+++ docs/home_assignment/delete_me.sql
@@ -2,3 +2,15 @@
 SELECT color
 FROM rectangles
 GROUP BY color;
+
+-- what did prof say about broken color hex value
+------------------------
+
+11.
+SELECT color
+FROM rectangles
+WHERE color IS NOT NULL
+GROUP BY color;
+
+
+-- what did prof say about broken color hex value
