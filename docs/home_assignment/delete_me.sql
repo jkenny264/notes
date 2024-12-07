206a9ac399cae173555749d26d79a724305ab7c0
jkenny264 <johnkenny264@gmail.com>
2024-12-06 23:25:25 -0500

    Update delete_me.sql

--- docs/home_assignment/delete_me.sql
+++ docs/home_assignment/delete_me.sql
@@ -9,8 +9,15 @@ GROUP BY color;
 11.
 SELECT color
 FROM rectangles
-WHERE color IS NOT NULL
+WHERE color IS NOT NULL AND color NOT LIKE '#%'
 GROUP BY color;
 
 
 -- what did prof say about broken color hex value
+
+-------------------------
+
+17.
+SELECT *
+FROM assignments
+WHERE due_date < CURRENT_DATE;
